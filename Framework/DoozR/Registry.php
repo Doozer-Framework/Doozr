e1198831412b37760949b25962ed9ba98c5f9452
Benjamin Carl <opensource@clickalicious.de>
2013-08-08 22:32:20 +0200

    added: New Registry (complete refactored)

--- Framework/DoozR/Registry.php
+++ Framework/DoozR/Registry.php
@@ -2,9 +2,9 @@
 /* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
 
 /**
- * DoozR Registry
+ * DoozR - Registry
  *
- * Registry.php - Registry of the DoozR Framework
+ * Registry.php - Registry of the DoozR framework.
  *
  * PHP versions 5
  *
@@ -17,16 +17,16 @@
  * modification, are permitted provided that the following conditions are met:
  *
  * - Redistributions of source code must retain the above copyright notice,
- *   this list of conditions and the following disclaimer.
+ * this list of conditions and the following disclaimer.
  * - Redistributions in binary form must reproduce the above copyright notice,
- *   this list of conditions and the following disclaimer in the documentation
- *   and/or other materials provided with the distribution.
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
  * - All advertising materials mentioning features or use of this software
- *   must display the following acknowledgement: This product includes software
- *   developed by Benjamin Carl and other contributors.
+ * must display the following acknowledgement: This product includes software
+ * developed by Benjamin Carl and other contributors.
  * - Neither the name Benjamin Carl nor the names of other contributors
- *   may be used to endorse or promote products derived from this
- *   software without specific prior written permission.
+ * may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -48,7 +48,7 @@
  * @author     Benjamin Carl <opensource@clickalicious.de>
  * @copyright  2005 - 2013 Benjamin Carl
  * @license    http://www.opensource.org/licenses/bsd-license.php The BSD License
- * @version    Git: $Id$
+ * @version    Git: $Id: 428927151fa092b4e706683a549ddfa910c88537 $
  * @link       http://clickalicious.github.com/DoozR/
  * @see        -
  * @since      -
@@ -58,9 +58,9 @@ require_once DOOZR_DOCUMENT_ROOT.'DoozR/Base/Class/Singleton.php';
 require_once DOOZR_DOCUMENT_ROOT.'DoozR/Registry/Interface.php';
 
 /**
- * DoozR Registry
+ * DoozR - Registry
  *
- * Registry of the DoozR Framework
+ * Registry of the DoozR framework.
  *
  * @category   DoozR
  * @package    DoozR_Core
@@ -68,99 +68,306 @@ require_once DOOZR_DOCUMENT_ROOT.'DoozR/Registry/Interface.php';
  * @author     Benjamin Carl <opensource@clickalicious.de>
  * @copyright  2005 - 2013 Benjamin Carl
  * @license    http://www.opensource.org/licenses/bsd-license.php The BSD License
- * @version    Git: $Id$
+ * @version    Git: $Id: e784ecf8d08dc686349ae8d5e80a0221bcdcc2ae $
  * @link       http://clickalicious.github.com/DoozR/
  * @see        -
  * @since      -
  */
-class DoozR_Registry extends DoozR_Base_Class_Singleton implements DoozR_Registry_Interface
+class DoozR_Registry extends DoozR_Base_Class_Singleton implements
+    DoozR_Registry_Interface,
+    ArrayAccess,
+    Iterator,
+    Countable
 {
-	/**
-	 * holds registry data
-	 *
-	 * @var array $data
-	 * @access protected
-	 */
-    protected $_data;
+    /**
+     * To be more flexible we use an array for storing properties
+     * which are passed via __set and set()
+     * key = property-name
+     *
+     * @var array
+     * @access protected
+     * @static
+     */
+    protected static $_lookup = array();
+
+    /**
+     * To be more flexible for a reverse lookup
+     * key = index (numeric)
+     *
+     * @var array
+     * @access protected
+     * @static
+     */
+    protected static $_reverseLookup = array();
+
+    /**
+     * Lookup matrix for implementation of ArrayAccess
+     * Those lookup matrix is used to retrieve the relation
+     * between an identifier and a numeric index
+     *
+     * @var array
+     * @access protected
+     * @static
+     */
+    protected static $_references = array();
+
+    /**
+     * The position of the iterator for iterating
+     * elements.
+     *
+     * @var integer
+     * @access protected
+     * @static
+     */
+    protected static $position = 0;
+
+    /**
+     * The count of elements precalculated for countable
+     * interface.
+     *
+     * @var integer
+     * @access protected
+     * @static
+     */
+    protected static $count = 0;
 
 
     /**
-     * adds value to registry
+     * This method stores an element in the registry under the
+     * passed key.
      *
-     * This method is intend to add a new value to the registry.
+     * @param string $variable   The variable (class, object) to store
+     * @param string $identifier The identifier for the stored object, class ...
+     *                           If not passed a UUID is calculated and returned
      *
-     * @param string $key   The key/identifier of the value
-     * @param mixed  $value The value to store/add
-     *
-     * @return  void
-     * @access  public
-     * @author  Benjamin Carl <opensource@clickalicious.de>
-     * @since   Method available since Release 1.0.0
-     * @version 1.0
+     * @author Benjamin Carl <opensource@clickalicious.de>
+     * @return string The identifier for reading the stored variable
+     * @access public
+     * @static
      */
-    public function __set($key, $value)
+    public static function set(&$variable, $identifier = null)
     {
-        $this->_data[$key] = $value;
-    }
-
-    /**
-     * returns value from registry
-     *
-     * This method is intend to return a value from the registry.
-     *
-     * @param string $key The key/identifier of the value
-     *
-     * @return  mixed|null
-     * @access  public
-     * @author  Benjamin Carl <opensource@clickalicious.de>
-     * @since   Method available since Release 1.0.0
-     * @version 1.0
-     */
-    public function __get($key)
-    {
-        if (isset($this->_data[$key])) {
-            return $this->_data[$key];
-        } else {
-            return null;
+        // generate identifier if not passed
+        if ($identifier === null) {
+            $identifier = sha1(serialize($variable));
         }
+
+        // store the variable as reference
+        self::$_references[]            = $variable;
+        $index                    = count(self::$_references)-1;
+        self::$_lookup[$identifier]     = $index;
+        self::$_reverseLookup[$index]   = $identifier;
+
+        // store count of elements
+        self::$count = $index+1;
+
+        // return identifier for outer use
+        return $identifier;
     }
 
     /**
-     * checks if a key is set
+     * This method returns a previously stored element from the registry
      *
-     * This method is intend to check if a key is set.
+     * @param string $identifier The identifier of the stored object, class ...
      *
-     * @param string $key The key/identifier to check
-     *
-     * @return  boolean TRUE if key is set, otherwise FALSE
-     * @access  public
-     * @author  Benjamin Carl <opensource@clickalicious.de>
-     * @since   Method available since Release 1.0.0
-     * @version 1.0
+     * @author Benjamin Carl <opensource@clickalicious.de>
+     * @return mixed The stored variable if exist
+     * @access public
+     * @static
      */
-    public function __isset($key)
+    public static function get($identifier = null)
     {
-        return isset($this->_data[$key]);
+        $result = null;
+
+        if ($identifier === null) {
+            $result = self::$_lookup;
+
+        } else {
+            if (isset(self::$_lookup[$identifier])) {
+                $result = self::$_references[self::$_lookup[$identifier]];
+
+            } else {
+                // simulate PHP's behavior by using custom error
+                $message = 'Undefined property: '.__CLASS__.'::'.$identifier;
+                $type    = E_USER_NOTICE;
+                trigger_error($message, $type);
+            }
+        }
+
+        return $result;
     }
 
     /**
-     * removes a key and its value from registry
+     * This method is a shortcut wrapper to set()
      *
-     * This method is intend to remove a key and its value from registry.
+     * @param string $identifier The identifier of the property
+     * @param mixed  $value      The value to store
      *
-     * @param string $key The key/identifier to remove
-     *
-     * @return  void
-     * @access  public
-     * @author  Benjamin Carl <opensource@clickalicious.de>
-     * @since   Method available since Release 1.0.0
-     * @version 1.0
+     * @author Benjamin Carl <opensource@clickalicious.de>
+     * @return mixed The result of the operation
+     * @access public
      */
-
-    public function __unset($key)
+    public function __set($identifier, $value)
     {
-        unset($this->_data[$key]);
+        return self::set($value, $identifier);
+    }
+
+    /**
+     * This method is a shortcut wrapper to get()
+     *
+     * @param string $identifier The identifier of the property
+     *
+     * @author Benjamin Carl <opensource@clickalicious.de>
+     * @return mixed The value of the property if exist
+     * @access public
+     */
+    public function __get($identifier)
+    {
+        return self::get($identifier);
+    }
+
+    /*******************************************************************************************************************
+     * Fulfill ArrayAccess interface requirements
+    \******************************************************************************************************************/
+
+    /**
+     * Returns the TRUE if the passed offset exists otherwise FALSE
+     *
+     * @param mixed $offset The offset to check
+     *
+     * @author Benjamin Carl <opensource@clickalicious.de>
+     * @return mixed The result of the operation
+     * @access public
+     */
+    public function offsetExists($offset)
+    {
+        return (isset(self::$_references[$offset]));
+    }
+
+    /**
+     * Returns the value for the passed offset
+     *
+     * @param mixed $offset The offset to return value for
+     *
+     * @author Benjamin Carl <opensource@clickalicious.de>
+     * @return mixed The result of the operation
+     * @access public
+     */
+    public function offsetGet($offset)
+    {
+        return self::$_references[$offset];
+    }
+
+    /**
+     * Sets the value for the passed offset
+     *
+     * @param integer $offset The offset to set value for
+     * @param mixed   $value  The value to write
+     *
+     * @author Benjamin Carl <opensource@clickalicious.de>
+     * @return mixed The result of the operation
+     * @access public
+     */
+    public function offsetSet($offset, $value)
+    {
+        self::$_references[$offset] = $value;
+
+    }
+
+    /**
+     * Unsets an offset
+     *
+     * @param mixed $offset The offset to unset
+     *
+     * @author Benjamin Carl <opensource@clickalicious.de>
+     * @return mixed The result of the operation
+     * @access public
+     */
+    public function offsetUnset($offset)
+    {
+        $identifier = self::$_reverseLookup[$offset];
+        unset(self::$_lookup[$identifier]);
+        unset(self::$_reverseLookup[$identifier]);
+        unset(self::$_references[$identifier]);
+    }
+
+    /*******************************************************************************************************************
+     * Fulfill Iterator interface requirements
+    \******************************************************************************************************************/
+
+    /**
+     * Rewinds the position to 0
+     *
+     * @author Benjamin Carl <opensource@clickalicious.de>
+     * @return mixed The result of the operation
+     * @access public
+     */
+    public function rewind()
+    {
+        self::$position = 0;
+    }
+
+    /**
+     * Checks if current position is still valid
+     *
+     * @author Benjamin Carl <opensource@clickalicious.de>
+     * @return mixed The result of the operation
+     * @access public
+     */
+    public function valid()
+    {
+        return self::$position < count(self::$_references);
+    }
+
+    /**
+     * Returns the key for the current position
+     *
+     * @author Benjamin Carl <opensource@clickalicious.de>
+     * @return mixed The result of the operation
+     * @access public
+     */
+    public function key()
+    {
+        return self::$position;
+    }
+
+    /**
+     * Returns the current element
+     *
+     * @author Benjamin Carl <opensource@clickalicious.de>
+     * @return mixed The result of the operation
+     * @access public
+     */
+    public function current()
+    {
+        return self::$_references[self::$position];
+    }
+
+    /**
+     * Goes to next element
+     *
+     * @author Benjamin Carl <opensource@clickalicious.de>
+     * @return mixed The result of the operation
+     * @access public
+     */
+    public function next()
+    {
+        self::$position++;
+    }
+
+    /*******************************************************************************************************************
+     * Fulfill Countable interface requirements
+    \******************************************************************************************************************/
+
+    /**
+     * Returns the count of elements in registry
+     *
+     * @author Benjamin Carl <opensource@clickalicious.de>
+     * @return mixed The result of the operation
+     * @access public
+     */
+    public function count()
+    {
+        return self::$count;
     }
 }
-
-?>
