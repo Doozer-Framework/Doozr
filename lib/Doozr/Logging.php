aea3deef1bdbd290a2ea3cd2b01631edab111055
Benjamin Carl <opensource@clickalicious.de>
2015-07-12 00:55:05 +0200

    Remove depedency for Psr Log

--- lib/Doozr/Logging.php
+++ lib/Doozr/Logging.php
@@ -58,7 +58,7 @@ require_once DOOZR_DOCUMENT_ROOT . 'Doozr/Logging/Abstract.php';
 require_once DOOZR_DOCUMENT_ROOT . 'Doozr/Logging/Interface.php';
 require_once DOOZR_DOCUMENT_ROOT . 'Doozr/Logging/Constant.php';
 
-use Psr\Log\LoggerAwareInterface;
+//use Psr\Log\LoggerAwareInterface;
 use Psr\Log\LoggerInterface;
 
 /**
@@ -84,8 +84,8 @@ final class Doozr_Logging extends Doozr_Logging_Abstract
     SplSubject,
     ArrayAccess,
     Iterator,
-    Countable,
-    LoggerAwareInterface
+    Countable//,
+    //LoggerAwareInterface
 {
     /**
      * The observer storage.
